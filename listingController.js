6f3c61e7b7d42e36742b2fbd3f817e35dfae98c5
Jonathen Settle <settlejonathen@yahoo.com>
2016-01-11 18:03:11 -0500

    First attempt at writing addListing and deleteListing function

--- listingController.js
+++ listingController.js
@@ -7,8 +7,12 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push($index);
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice($index,1);
+    };
     $scope.showDetails = function(index) {};
   }
 ]);
