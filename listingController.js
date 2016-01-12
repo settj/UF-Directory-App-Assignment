23d8e840e7f13c621c23313b83d0acc77267755a
Jonathen Settle <settlejonathen@yahoo.com>
2016-01-11 19:47:43 -0500

    Initial version of showDetails written

--- listingController.js
+++ listingController.js
@@ -13,6 +13,25 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.deleteListing = function(index) {
       $scope.listings.splice(index,1);
     };
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = {
+            name : $scope.listings[index].name,
+            latitude : "", 
+            longitude : "",  
+            address : "",
+            description: "",
+        };
+
+      if ($scope.listings[index].coordinates == undefined && $scope.listings[index].address == undefined) {
+        $scope.detailedInfo.description = "No detailed information available";
+      };
+      if (!($scope.listings[index].coordinates == undefined)) {
+        $scope.detailedInfo.latitude = $scope.listings[index].coordinates.latitude + '\n';
+        $scope.detailedInfo.longitude = $scope.listings[index].coordinates.longitude + '\n';
+      };
+      if (!($scope.listings[index].address == undefined)) {
+        $scope.detailedInfo.address = $scope.listings[index].address;
+      };
+    };
   }
 ]);
